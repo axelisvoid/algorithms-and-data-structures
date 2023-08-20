fee036efbabc2a30b48d02f3ac10959523a57f90
Axel Rodríguez Chang <axelrogg@icloud.com>
2023-08-20 22:52:34 +0200

    Add doubly linked list implementation

--- dss/linked_list/py/doubly_linked_list.py
+++ dss/linked_list/py/doubly_linked_list.py
@@ -1,22 +1,21 @@
 from typing import Generator
 
-
 class Node:
     def __init__(self, key: int) -> None:
         self.key = key
         self.next: Node | None = None
-    
+        self.prev: Node | None  = None
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self) -> None:
         self.head: Node | None = None
-
+    
     def __iter__(self) -> Generator[Node, None, None]:
         node = self.head
         while node:
             yield node
             node = node.next
-    
+
     def __str__(self) -> str:
         return "->".join([f"{node.key}" for node in self])
 
@@ -25,46 +24,34 @@ class LinkedList:
         for _ in self:
             counter += 1
         return counter
-    
-    def build(self, keys: list[int]) -> None:
+
+    def build(self, keys: list[Node]) -> None:
         for key in keys:
             self.insert_last(key)
-
+    
     def insert_first(self, key: int) -> None:
         new_node = Node(key)
-        if not self.head:
+        if self.head is None:
             self.head = new_node
             return
         new_node.next = self.head
         self.head = new_node
-            
+    
     def insert_last(self, key: int) -> None:
         new_node = Node(key)
         skip = 0
-        if not self.head:
+        if self.head is None:
             self.head = new_node
             skip += 1
         
         current_node = self.head
         for _ in range(len(self) - skip):
-            if not current_node.next:
+            if current_node.next is None:
                 current_node.next = new_node
             current_node = current_node.next
 
-    def insert_at(self, index: int, key: int) -> None:
-        raise NotImplementedError
-
-    def get_at(self, i: int) -> None:
-        raise NotImplementedError
-
-    def set_at(self, i: int, key: int) -> None:
-        raise NotImplementedError
-
-    def get_by_key(self, key: int) -> None:
-        raise NotImplementedError
-
-    def delete_by_key(self, key: int) -> None:
-        raise NotImplementedError
-    
-    def reverse(self) -> None:
-        raise NotImplementedError
+    def get_by_key(self, key: int) -> Node | None:
+        for node in self:
+            if node.key == key:
+                return node
+        return None
